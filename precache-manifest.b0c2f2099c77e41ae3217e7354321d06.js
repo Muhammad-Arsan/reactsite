a3432ab150f231fae0a722304baddb5d97af3752
Muhammad-Arsan <arsan924@gmail.com>
2020-09-05 20:40:35 +0500

    Updates

---
+++ precache-manifest.b0c2f2099c77e41ae3217e7354321d06.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5faba22fcc2dd207f14dd60c4999b94f",
+    "url": "/reactsite/index.html"
+  },
+  {
+    "revision": "163901d00b025a042f04",
+    "url": "/reactsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "d25190e33e675c2dc29a",
+    "url": "/reactsite/static/css/main.02b9d76f.chunk.css"
+  },
+  {
+    "revision": "163901d00b025a042f04",
+    "url": "/reactsite/static/js/2.efe22dc9.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactsite/static/js/2.efe22dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d25190e33e675c2dc29a",
+    "url": "/reactsite/static/js/main.2272fa31.chunk.js"
+  },
+  {
+    "revision": "12b08153ee314441ded6",
+    "url": "/reactsite/static/js/runtime-main.ba80d13b.js"
+  },
+  {
+    "revision": "1d835de4cb7b1f71f264cec66809d1cc",
+    "url": "/reactsite/static/media/code.1d835de4.svg"
+  },
+  {
+    "revision": "2bd44d1de825cbd46288b3d9bcf9b504",
+    "url": "/reactsite/static/media/coding.2bd44d1d.svg"
+  }
+]);
